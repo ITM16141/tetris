565cb2a5d3af0175f1f1f11629c7fc98c128d21d
ITM16141 <i.atsuki611@gmail.com>
2025-12-12 09:16:50 +0900

    不要なコメントを削除

--- server.py
+++ server.py
@@ -90,7 +90,6 @@ def handle_client(conn, addr):
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # 宛先は実在しなくてよい（接続は発生しない）
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
     except:
